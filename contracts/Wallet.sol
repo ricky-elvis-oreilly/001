d6d5a429977dfd31370c63058e51b020819fe551
Ricky Elvis O'Reilly <ricky.elvis.oreilly@hotmail.com>
2023-02-26 11:43:57 +0300

    ...

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -1,3 +1,30 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.7;
+
+contract Wallet is SettingsClient {
+  uint256 numSettingsToMakeATransfer;
+  mapping (uint256 => Settings.SettingToMakeATransfer) SettingToMakeATransfer;
+
+
+  event HandleLayerStarted();
+  event HandleLayerSuccess();
+  event HandleLayerFailure();
+
+
+  function handleLayerStarted() public {
+    emit HandleLayerStarted();
+  }
+
+  function handleLayerSuccess() public {
+    emit HandleLayerSuccess();
+  }
+
+  function handleLayerFailure() public {
+    emit HandleLayerFailure();
+  }
+
+
+  function test() public {
+  }
+}
